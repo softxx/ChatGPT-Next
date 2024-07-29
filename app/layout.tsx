8a16e2c37c16f4a02fabe6973b44689a927d0dc4
寻找一束光 <6@6-2.vip>
2024-07-29 19:01:18 +0800

    修改网站名称

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "Light AI",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "Light AI",
     statusBarStyle: "default",
   },
 };
